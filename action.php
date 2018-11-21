0a4d1f5172964ab4f554973e90b097313d0772c1
Elisee Mizkael <elisee.kalonji99@gmail.com>
2018-11-21 13:11:25 +0200

    Premier pas dans le fichier action.php

---
+++ action.php
@@ -0,0 +1,34 @@
+<?php
+//---------poo---------------------------------
+class Action{
+    const CITY="LSHI";
+    public $nom;    
+    public $address;
+    public $heurdeb;
+    public $heurfin;    
+    public function info(){
+        echo "Info par rapport au visite";
+        $now=getdate();
+        $hour=$now['hours'];
+        $jour=$now['wday'];
+        if(($hour>=$this->heurdeb)&&($hour<=$this->heurfin)){
+            echo "<br/>L'hopital est ouvert!";
+        }
+        else{
+            echo "<br/>L'hopital est ferme";
+            echo '<br/>'.$hour.'<br/>'.$jour;
+        }
+    }
+}
+$maclass="Action";
+$hospital=new $maclass();
+$hospital->nom="Sendwe";
+$hospital->address="123.Av sendwe Q.makomeno C.L'shi";
+$hospital->heurdeb=21;
+$hospital->heurfin=24;
+$hospital->info();echo '<br/>';
+var_dump($hospital);
+$str=gettype($hospital);
+if(!($hospital instanceof $maclass)){
+    echo '<br/>Ok';
+}
