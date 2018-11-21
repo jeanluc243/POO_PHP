6783a2c8e61f61f1486acf239b1611bcdd096b49
Elisee Mizkael <elisee.kalonji99@gmail.com>
2018-11-21 13:35:03 +0200

    Ajout d'une premiere class derivee

--- heritage.php
+++ heritage.php
@@ -12,4 +12,18 @@
             return $info;
         }
     }
+     //class derivee
+     class Action extends Valeur{
+        public $bourse;
+        public function __construct($nom,$prix,$bourse){
+            parent::__construct($nom,$prix);//utilise le constructeur du parrent
+            $this->bourse=$bourse;
+        }
+	//redefinition
+        public function getinfo(){
+            $info="<h3>Action $this->nom cotee a la bourse $this->bourse</h3>";
+            $info.=parent::getinfo();
+            return $info;
+        }
+    }
 ?>
