704fc39b2c36e0063ca692b1358111c75856d7a3
jeanluc kabulu <jeanluc.kabulu@gmail.com>
2018-11-30 07:30:54 +0200

    Update construct.php

--- construct.php
+++ construct.php
@@ -2,23 +2,27 @@
 
 //-------------Class avec constructeur----------------------
 
-    class Person{
+    class Person
+    {
         private $nom;
         private $email;
         private $tel;
         private $nb_instance=0;
 
-        public function __construct($n,$email,$tel){
+        public function __construct($n,$email,$tel)
+        {
             $this->nom=$n;
             $this->email=$email;
             $this->tel=$tel;
             $this->nb_instance++;
         }
+        
         public function affiche(){
             echo "$this->nom<br/>";
             echo "$this->email<br/>";
             echo "$this->tel<br/>";
         }
+        
         public function __destruct(){
             echo "<br/>La personne $this->nom est effacee<br/>";
         }
