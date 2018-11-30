71cc2a2248b674221d14d496989794065e2c7b25
jeanluc kabulu <jeanluc.kabulu@gmail.com>
2018-11-30 06:24:09 +0200

    Update acces.php

--- acces.php
+++ acces.php
@@ -1,11 +1,14 @@
 <?php
 
-    class Acces{
+    class Acces
+    {
+    
         public $varpub="Varpub";
         protected $varpro="Varpro";
         private $varpriv="Varpriv";
 
-        public function lirevar(){
+        public function lirevar()
+        {
             echo "$this->varpub<br/>";
             echo "$this->varpro<br/>";
             echo "$this->varpriv<br/>";
@@ -15,26 +18,37 @@
     $maclasse="Acces";
     $ac1=new $maclasse();
     $ac1->lirevar();
+    
     //echo "<br/>$ac1->varpub";
     //echo "<br/>$ac1->varpro";
     //echo "<br/>$ac1->varpriv";
+
 ?>
+
 <!--Accessibilite des methodes-->
+
 <?php
 
-    class Accesmeth{
+    class Accesmeth
+    {
         private $nom="Elisee";
-        public function lirepub(){
+        public function lirepub()
+        {
             echo "$this->nom<br/>";
             $this->lirepro();
             $this->lirepriv();
         }
-        protected function lirepro(){
+        
+        protected function lirepro()
+        {
             echo "$this->nom<br/>";
         }
-        private function lirepriv(){
+        
+        private function lirepriv()
+        {
             echo "$this->nom<br/>";
         }
+        
     }
 
     $ac2=new Accesmeth();
@@ -42,20 +56,27 @@
     //$ac2->lirepro(); impossible
     //$ac2->lirepriv(); impossible
 ?>
+
 <!--proprietes et methodes static-->
+
 <?php
 
-    class Metstat{
+    class Metstat
+    {
         public static $nom="Mizkael";
-        public static function getheur(){
+        public static function getheur()
+        {
             $heure=date("h : m : s");
             return $heure;
         }
-        protected static function affiche(){
+        
+        protected static function affiche()
+        {
             $text=Metstat::$nom.' il est '.Metstat::getheur();
             return $text;
         } 
     }
+
     echo '<br/>'.Metstat::$nom.'<br/>';
     echo Metstat::getheur().'<br/>';
     echo Metstat::affiche();
